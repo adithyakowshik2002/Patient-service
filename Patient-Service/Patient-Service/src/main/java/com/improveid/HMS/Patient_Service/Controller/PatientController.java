44e5d508578d89d3f7d337c7ab758c2c993b6dd7
GannevaramAkhil <adithyakowshik2002@gmail.com>
2025-04-15 16:51:18 +0530

    Added slf4j PatientController

--- Patient-Service/Patient-Service/src/main/java/com/improveid/HMS/Patient_Service/Controller/PatientController.java
+++ Patient-Service/Patient-Service/src/main/java/com/improveid/HMS/Patient_Service/Controller/PatientController.java
@@ -7,6 +7,7 @@ import com.improveid.HMS.Patient_Service.Enums.BloodGroup;
 import com.improveid.HMS.Patient_Service.Exception.PatientNotFoundException;
 import com.improveid.HMS.Patient_Service.Service.PatientService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,67 +15,64 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@Slf4j
 @RestController
 @RequestMapping("/api/patients")
 public class PatientController {
 
 
     @Autowired
-    private  PatientService patientService;
+    private PatientService patientService;
 
     @PostMapping
-    public ResponseEntity<PatientResponse> addPatient(@Valid @RequestBody PatientRequest patientRequest){
-
-       PatientResponse addPatient = patientService.addPatient(patientRequest);
-        return new ResponseEntity<>(addPatient,HttpStatus.CREATED);
+    public ResponseEntity<PatientResponse> addPatient(@Valid @RequestBody PatientRequest patientRequest) {
+        log.info("Adding new patient");
+        PatientResponse addPatient = patientService.addPatient(patientRequest);
+        return new ResponseEntity<>(addPatient, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<List<PatientResponse>> getALlPatients(){
+    public ResponseEntity<List<PatientResponse>> getALlPatients() {
         List<PatientResponse> Patients = patientService.getAllPatients();
-        return new ResponseEntity<>(Patients,HttpStatus.OK);
+        return new ResponseEntity<>(Patients, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<PatientResponse> getPatientById(@PathVariable Integer id) throws PatientNotFoundException {
-       PatientResponse response = patientService.getPatientById(id);
-        return new ResponseEntity<>(response,HttpStatus.OK);
+        PatientResponse response = patientService.getPatientById(id);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<PatientResponse> updatePatient(@PathVariable Integer id, @RequestBody PatientRequest patientRequest){
-        PatientResponse updatePatient = patientService.updatePatient(id,patientRequest);
-        return new ResponseEntity<>(updatePatient,HttpStatus.OK);
+    public ResponseEntity<PatientResponse> updatePatient(@PathVariable Integer id, @RequestBody PatientRequest patientRequest) {
+        PatientResponse updatePatient = patientService.updatePatient(id, patientRequest);
+        return new ResponseEntity<>(updatePatient, HttpStatus.OK);
     }
 
     @GetMapping("/aadhar/{aadhar}")
-    public ResponseEntity<PatientResponse> getPatientByAadhar(@PathVariable String aadhar){
+    public ResponseEntity<PatientResponse> getPatientByAadhar(@PathVariable String aadhar) {
         PatientResponse patientResponse = patientService.getPatientByAadhar(aadhar);
-        return new ResponseEntity<>(patientResponse,HttpStatus.OK);
+        return new ResponseEntity<>(patientResponse, HttpStatus.OK);
     }
 
     @GetMapping("/email/{email}")
-    public ResponseEntity<PatientResponse> getPatientByEmail(@PathVariable String email){
+    public ResponseEntity<PatientResponse> getPatientByEmail(@PathVariable String email) {
         PatientResponse patientResponse = patientService.getPatientByEmail(email);
-        return new ResponseEntity<>(patientResponse,HttpStatus.OK);
+        return new ResponseEntity<>(patientResponse, HttpStatus.OK);
     }
 
     @GetMapping("/bloodgroup/{bloodGroup}")
-    public ResponseEntity<List<PatientResponse>> getPatientsByBloodGroup(@PathVariable BloodGroup bloodGroup){
+    public ResponseEntity<List<PatientResponse>> getPatientsByBloodGroup(@PathVariable BloodGroup bloodGroup) {
         List<PatientResponse> patients = patientService.getPatientsByBloodGroup(bloodGroup);
 
-        return new ResponseEntity<>(patients,HttpStatus.OK);
+        return new ResponseEntity<>(patients, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePatient(@PathVariable Integer id) {
         patientService.deletePatient(id);
-        return new ResponseEntity<>("Deleted Successfully",HttpStatus.OK);
+        return new ResponseEntity<>("Deleted Successfully", HttpStatus.OK);
     }
-
-
-
 
 
 }
